70d103874a1997f9c4be4ec225a5a753e6a1f837
user <thdjun92@naver.com>
2025-09-20 13:26:51 +0900

    변수 공부

---
+++ src/variable/Main.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        int b = 10;
+        //System.out.println(a); 초기화안하면 에러
+        a = 11;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.printf("이름: %s", "코찬이");
+        System.out.printf("이름: %d", 21);
+    }
+}
