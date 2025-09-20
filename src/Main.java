ac5b4b20adf74970490c4212d667952eb2b3b0e5
user <thdjun92@naver.com>
2025-09-20 13:21:42 +0900

    init

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("Hello");
+        System.out.println("Java");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt());
+        sc.close();
+    }
+}
